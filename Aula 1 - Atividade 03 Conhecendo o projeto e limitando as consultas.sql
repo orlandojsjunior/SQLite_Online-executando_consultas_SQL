c2b5e129a2adfa8a4edcadba84026a9d4ed0d505
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-04 00:50:01 -0300

    UPDATE - Conhecendo o projeto e limitando as consultas

--- Aula 1 - Atividade 03 Conhecendo o projeto e limitando as consultas.sql
+++ Aula 1 - Atividade 03 Conhecendo o projeto e limitando as consultas.sql
@@ -1,1 +1,19 @@
 SELECT * FROM Colaboradores;
+
+
+SELECT * FROM HistoricoEmprego;
+
+-- identificar as cinco pessoas colaboradoras que possuem as maiores remunerações?
+
+-- menor salário até o maior
+SELECT * FROM HistoricoEmprego
+ORDER BY salario;
+
+-- Maior salário até o menor
+SELECT * FROM HistoricoEmprego
+ORDER BY salario DESC;
+
+--  limitarmos o número de linhas do nosso resultado
+SELECT * FROM HistoricoEmprego
+ORDER BY salario DESC
+LIMIT 5;
