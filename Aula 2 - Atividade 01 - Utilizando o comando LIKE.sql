c927ecd6c72f7a59c7bfb72d6484a5c81835d59a
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-05 22:55:19 -0300

    Utilizando o comando LIKE

---
+++ Aula 2 - Atividade 01 - Utilizando o comando LIKE.sql
@@ -0,0 +1,15 @@
+/* Utilizando operadores */
+
+-- Visualização dos cinco primeiros registros da tabela de treinamento. 
+SELECT * FROM Treinamento 
+
+
+-- Filtrando por palavras no começo de um nome.
+
+SELECT * FROM Treinamento
+WHERE curso = 'O poder';
+
+-- Utilizando o comando LIKE.
+
+SELECT * FROM Treinamento
+WHERE curso LIKE 'O poder%';
