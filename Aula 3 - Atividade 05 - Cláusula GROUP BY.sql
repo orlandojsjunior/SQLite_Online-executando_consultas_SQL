95c70f2cd586ef657222ce4c928440c2e59ac8aa
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-09 20:11:31 -0300

    Cláusula GROUP BY

--- Aula 3 - Atividade 05 - Cláusula GROUP BY.sql
+++ Aula 3 - Atividade 05 - Cláusula GROUP BY.sql
@@ -3,10 +3,9 @@
 SELECT parentesco FROM Dependentes
 GROUP BY parentesco;
 
-
 -- quantidade de registros que temos para cada tipo de parentesco
 
-SELECT parentesco, COUNT(*) FROM Dependentes
+SELECT parentesco, COUNT(*) AS 'Quantidades' FROM Dependentes
 GROUP BY parentesco;
 
 /*
@@ -25,4 +24,10 @@ GROUP BY instituicao;
 SELECT instituicao, COUNT(curso)
 FROM Treinamento
 GROUP BY instituicao
-WHERE COUNT(curso) > 2;
+WHERE COUNT(curso) > 2; -- neste caso vai da erro de sintax
+
+
+/*
+Quando estamos trabalhando com funções de agregação, não conseguimos utilizar o WHERE. 
+Precisamos de uma cláusula específica que faça a mesma função do WHERE para trabalhar com essas funções de agregação.
+*/
