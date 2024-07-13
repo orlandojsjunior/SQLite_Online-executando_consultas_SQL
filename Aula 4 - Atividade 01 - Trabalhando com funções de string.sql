9f3cb70ed8464fb4f9a648712e0b2f273746ae00
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-12 22:30:02 -0300

    Update - Trabalhando com funções de string

--- Aula 4 - Atividade 01 - Trabalhando com funções de string.sql
+++ Aula 4 - Atividade 01 - Trabalhando com funções de string.sql
@@ -1,11 +1,19 @@
 -- Verificando os CPFs com a função LENGTH
-
 -- Atenção: Em alguns sistemas, essa função se chama LEN. Verifique a documentação do SGBD como essa função funciona.
+
+SELECT nome, LENGTH(cpf) qtd
+FROM Colaboradores
+WHERE qtd = 11;
+
+-- conferindo se todo os colaboradores estao com o CPF preenchido de forma correta
 
 SELECT COUNT(*), LENGTH(cpf) qtd
 FROM Colaboradores
 WHERE qtd = 11;
 
+/*
+ A função LENGTH é uma ferramenta útil em SQL para determinar o tamanho de uma cadeia de caracteres (string) ou o número de bytes em um valor binário
+*/ 
 
 -- Criando pequenos textos com a função CONCAT
 
@@ -14,7 +22,7 @@ A próxima demanda que a Fokus trouxe é que ela gostaria de ver os dados das pe
 sem parecer muito uma tabela. Como se fosse um pequeno texto falando de cada pessoa colaboradora e é possível fazer isso utilizando a linguagem SQL.
 */
 
-SELECT (' pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
+SELECT ('A pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
             || endereco) AS texto
             FROM Colaboradores;
 
@@ -23,14 +31,14 @@ SELECT (' pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o segui
 
 -- A função UPPER deixa todos os caracteres de texto maiúsculos.
 
-SELECT UPPER(' pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
+SELECT UPPER('A pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
             || endereco) AS texto
             FROM Colaboradores;
 
 
 -- A função LOWER, por outro lado, transforma todas as letras em minúscula.
 
-SELECT LOWER(' pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
+SELECT LOWER('A pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
             || endereco) AS texto
             FROM Colaboradores;
 
