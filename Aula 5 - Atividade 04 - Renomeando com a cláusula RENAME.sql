afc2d904bb76ca361257b1e0f4ecc08aeaf9bab1
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-15 22:56:18 -0300

    Cláusula RENAME

---
+++ Aula 5 - Atividade 04 - Renomeando com a cláusula RENAME.sql
@@ -0,0 +1,11 @@
+-- Usando a cláusula rename
+
+-- mudar o nome de uma tabela específica do banco de dados, que é a de HistoricoEmprego
+
+
+ALTER TABLE HistoricoEmprego RENAME TO CargosColaboradores;
+
+-- Deve seguir as Boas Práticas de Nomenclatura de Tabelas e Colunas de acordo a empresa em que você trabalha.
+
+
+
