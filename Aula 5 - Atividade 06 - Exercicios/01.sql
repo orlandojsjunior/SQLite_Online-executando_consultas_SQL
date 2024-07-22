58c2e7a2db2b20a80e5998370f8ec4c33ec3018a
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-22 15:53:26 -0300

    Selecionando os primeiros 5 registros

--- Aula 5 - Atividade 06 - Exercicios/01.sql
+++ Aula 5 - Atividade 06 - Exercicios/01.sql
@@ -1,9 +1,8 @@
--- 1. Selecione os primeiros 5 registros da tabela Colaboradores, ordenando-os pelo nome em ordem crescente.
-
+-- 1. Selecione os primeiros 5 registros da tabela clientes, ordenando-os pelo nome em ordem crescente. 
 
 SELECT
     *
 FROM
-    COLABORADORES
+    clientes
 ORDER BY
     NOME ASC LIMIT 5;
