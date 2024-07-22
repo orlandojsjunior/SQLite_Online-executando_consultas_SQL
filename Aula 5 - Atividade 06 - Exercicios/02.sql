9d540f49b5481f003bc6740e130234cf9a98bf2f
Orlando Junior <orlandojsjunior@hotmail.com>
2024-07-21 21:13:30 -0300

    Exercicio 02

---
+++ Aula 5 - Atividade 06 - Exercicios/02.sql
@@ -0,0 +1,6 @@
+-- Encontre todos os produtos na tabela produtos que não têm uma descrição associada (suponha que a coluna de descrição possa ser nula).
+
+SELECT
+    *
+FROM
+    TREINAMENTO WHRE IS NULL;
